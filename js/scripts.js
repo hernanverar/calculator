d98f6f53b60961f94b1c9aea200e7ed80f034935
Hernan Verar <hernanverar@gmail.com>
2023-01-09 14:30:26 -0800

    Set up scrips files Js
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+function saySomething(whatToSay) {
+    window.alert(whatToSay);
+  }
+  function add(number1, number2) {
+    return number1 + number2;
+}
+
+
+
+
+const result= add(2,6);
+const outputText="the sum is " + result + ".";
+saySomething(outputText)
