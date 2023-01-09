532c67bb635e0230ad3bb0516533a5f6927543c1
Hernan Verar <hernanverar@gmail.com>
2023-01-09 15:24:37 -0800

    WIP calculator add user responces
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,62 @@
-function saySomething(whatToSay) {
-    window.alert(whatToSay);
-  }
-  function add(number1, number2) {
+// function saySomething(whatToSay) {
+//     window.alert(whatToSay);
+//   }
+//   function add(number1, number2) {
 
-    return number1 + number2;
+//     return number1 + number2;
+
+// }
+
+// function subtract(number1, number2) {
+
+//   return number1 - number2;
+
+// }
+// function multiply(number1, number2) {
+
+//   return number1 * number2;
+
+// }
+// function divide(number1, number2) {
+
+//   return number1 / number2;
+
+// }
+
+// let response1 = parseInt(prompt("Enter a number:"));
+// let response2 = parseInt(prompt("Enter another number:"));
+// saySomething('The addition of your numbers equals ' + add(response1,response2) + '. The subtration of your numbers equals ' + subtract(response1,response2) + '. The division of your numbers equals ' + divide(response1,response2) + '. And the multiplication of your numbers equals ' + multiply(response1,response2)+ '.');
+// // saySomething(subtract(1, 6));
+// // saySomething(multiply(2, 4));
+// // saySomething(divide(2, 12));
+// // saySomething(add(response1,response2));
+
+function saySomething(response1,response2) 
+{
+ window.alert('The addition of your numbers equals ' + add(response1,response2) + '. The subtration of your numbers equals ' + subtract(response1,response2) + '. The division of your numbers equals ' + divide(response1,response2) + '. And the multiplication of your numbers equals ' + multiply(response1,response2)+ '.');
+}
+function add(number1, number2) {
+
+  return number1 + number2;
 
 }
 
 function subtract(number1, number2) {
 
-  return number1 - number2;
+return number1 - number2;
 
 }
 function multiply(number1, number2) {
 
-  return number1 * number2;
+return number1 * number2;
 
 }
 function divide(number1, number2) {
 
-  return number1 / number2;
+return number1 / number2;
 
 }
 
 let response1 = parseInt(prompt("Enter a number:"));
 let response2 = parseInt(prompt("Enter another number:"));
-saySomething('The addition of your numbers equals ' + add(response1,response2) + '. The subtration of your numbers equals ' + subtract(response1,response2) + '. The division of your numbers equals ' + divide(response1,response2) + '. And the multiplication of your numbers equals ' + multiply(response1,response2)+ '.');
-// saySomething(subtract(1, 6));
-// saySomething(multiply(2, 4));
-// saySomething(divide(2, 12));
-// saySomething(add(response1,response2));
+saySomething(response1,response2);
