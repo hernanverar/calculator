bc0a135ac9806355b71d0baa2666880a96d6e5eb
Hernan Verar <hernanverar@gmail.com>
2023-01-09 15:14:24 -0800

    Add funtionallity to numbers.
    Co-authored-by: johnedisc <christopher.johnedis@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,31 @@ function saySomething(whatToSay) {
     window.alert(whatToSay);
   }
   function add(number1, number2) {
+
     return number1 + number2;
+
 }
 
+function subtract(number1, number2) {
 
+  return number1 - number2;
 
+}
+function multiply(number1, number2) {
 
-const result= add(2,6);
-const outputText="the sum is " + result + ".";
-saySomething(outputText)
+  return number1 * number2;
+
+}
+function divide(number1, number2) {
+
+  return number1 / number2;
+
+}
+
+let response1 = parseInt(prompt("Enter a number:"));
+let response2 = parseInt(prompt("Enter another number:"));
+saySomething('The addition of your numbers equals ' + add(response1,response2) + '. The subtration of your numbers equals ' + subtract(response1,response2) + '. The division of your numbers equals ' + divide(response1,response2) + '. And the multiplication of your numbers equals ' + multiply(response1,response2)+ '.');
+// saySomething(subtract(1, 6));
+// saySomething(multiply(2, 4));
+// saySomething(divide(2, 12));
+// saySomething(add(response1,response2));
